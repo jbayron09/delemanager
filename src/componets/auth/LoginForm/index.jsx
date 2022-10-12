b1b1e84c9ae87e32d44a75e7f6eee6dadac5dd9f
jbayron09 <j.bayron.b@gmail.com>
2022-10-12 11:44:40 -0500

    'Rendering login form'

---
+++ src/componets/auth/LoginForm/index.jsx
@@ -0,0 +1,12 @@
+
+export const LoginForm = () => {
+    return (
+        <form action="">
+            <input type="text" placeholder="Correo electrónico"/>
+            <input type="text" placeholder="Contraseña"/>
+            <button>Ingresar</button>
+        </form>
+    )
+}
+
+export default LoginForm

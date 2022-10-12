f936712b47ec05107569756b608c686cca4de05f
JatsukinG <jatmolina15@gmail.com>
2022-10-12 14:58:25 -0500

    modified logo size props
    added default value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import {Logo} from "./components/main/Logo";
 
 function App() {
     return (
         <div className="App">
-            <Logo size="sm"/>
+
         </div>
     );
 }

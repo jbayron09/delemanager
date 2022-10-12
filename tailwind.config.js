072e46b7947f387e08ea7eb8d72b096471af6013
JatsukinG <jatmolina15@gmail.com>
2022-10-12 11:33:40 -0500

    install tailwind and setup gray colors preferences

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          50: '#F5F7FA',
+          100: '#E4E7EB',
+          200: '#CBD2D9',
+          300: '#9AA5B1',
+          400: '#7B8794',
+          500: '#616E7C',
+          600: '#52606D',
+          700: '#3E4C59',
+          800: '#323F4B',
+          900: '#1F2933',
+        },
+      },
+    },
+  },
+  plugins: [],
+}

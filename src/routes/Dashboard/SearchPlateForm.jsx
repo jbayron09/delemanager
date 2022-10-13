66ac608de9c038521eb497352289278063692e4b
JatsukinG <jatmolina15@gmail.com>
2022-10-13 17:46:53 -0500

    added input validation

--- src/routes/Dashboard/SearchPlateForm.jsx
+++ src/routes/Dashboard/SearchPlateForm.jsx
@@ -1,7 +1,27 @@
 import {Button} from "components/main/Button";
+import {useState} from "react";
 
 export function SearchPlateForm() {
+    const [inputValue, setInputValue] = useState('')
+    const [deleteKey, setDeleteKey] = useState(false)
 
+    const handleKeyDelete = (e) => e.key === 'Backspace' && setDeleteKey(true)
+
+    const handleInputValue = (e) => {
+        if (e.target.value.length <= 7) {
+            if (deleteKey === true && e.target.value.length === 3) {
+                setInputValue(e.target.value.substring(0, 2))
+            } else {
+                let vehiclePlate = e.target.value.toUpperCase()
+                if (e.target.value.length === 3) {
+                    vehiclePlate = vehiclePlate + ' '
+                }
+                if (e.target.value.length <= 2) {
+                }
+                setInputValue(vehiclePlate)
+            }
+        }
+    }
 
     return (
         <form className="flex flex-col px-5">
@@ -9,6 +29,9 @@ export function SearchPlateForm() {
                 className="rounded-md text-center text-6xl text-gray-800 font-bold mb-3 placeholder:text-gray-200 border-gray-100"
                 type="text"
                 placeholder="PLACA"
+                value={inputValue}
+                onChange={handleInputValue}
+                onKeyDown={handleKeyDelete}
             />
             <Button
                 fullWidth>

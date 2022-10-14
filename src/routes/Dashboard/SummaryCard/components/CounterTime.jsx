0e74e21c0f84158dc440f09dcba85ce1d871f823
jbayron09 <j.bayron.b@gmail.com>
2022-10-14 17:22:38 -0500

    both hours and minutes are shown only if hours > 0, the corresponding limit is also added to each of the represented diff.seconds, the code is reorganized respecting the spaces

--- src/routes/Dashboard/SummaryCard/components/CounterTime.jsx
+++ src/routes/Dashboard/SummaryCard/components/CounterTime.jsx
@@ -6,7 +6,7 @@ export default function CounterTime() {
 
     const calculateDiff = useCallback(() => {
         const date1 = DateTime.now()
-        const date2 = DateTime.fromISO("2022-10-14T12:00:00")
+        const date2 = DateTime.fromISO("2022-10-14T17:12:00")
         setDiff(date1.diff(date2, ["months", "days", "hours", "minutes", "seconds"]).toObject())
     })
 
@@ -14,17 +14,17 @@ export default function CounterTime() {
         const timeout = setTimeout(calculateDiff, 1000)
 
         return () => clearTimeout(timeout)
-    },[calculateDiff])
+    }, [calculateDiff])
 
     return (diff &&
         <>
             {
-                diff.hours +
-                (diff.hours === 1 ? " hora, " : " horas, ") +
-                diff.minutes +
-                (diff.minutes === 1 ? " minuto, " : " minutos, ") +
+                (diff.hours > 0 ?
+                    diff.hours + (diff.hours === 1 ? " hora, " : " horas, ") : "") +
+                (diff.minutes > 0 ?
+                    diff.minutes + (diff.minutes === 1 ? " minuto, " : " minutos, ") : "") +
                 Math.ceil(diff.seconds) +
-                (diff.seconds === 1 ? " segundo" : " segundos")
+                (Math.ceil(diff.seconds) === 1 ? " segundo" : " segundos")
             }
         </>)
 }

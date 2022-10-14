7b010c2ffe8fa16add253263eb3745a822757487
JatsukinG <jatmolina15@gmail.com>
2022-10-14 10:58:05 -0500

    created button delete

--- src/routes/Dashboard/Dashboard.jsx
+++ src/routes/Dashboard/Dashboard.jsx
@@ -1,17 +1,23 @@
 import Header from "components/main/Header";
 import SearchPlateForm from "routes/Dashboard/SearchPlateForm";
 import SummaryCard from "routes/Dashboard/SummaryCard";
+import {useState} from "react";
 
 export default function Dashboard(){
+    const [showSummary, setShowSummary] = useState(false)
+    const handleSearchPlate = () => {
+        setShowSummary(true)
+    }
+
     return (
         <>
             <Header/>
             <div className="px-4">
                 <h3 className="my-4 text-gray-700 text-sm font-bold">Parqueadero los 3 elefantes</h3>
                 <div className="mb-4">
-                    <SearchPlateForm/>
+                    <SearchPlateForm handleSearchPlate={handleSearchPlate}/>
                 </div>
-                <SummaryCard/>
+                {showSummary && <SummaryCard />}
             </div>
         </>
     )
--- src/routes/Dashboard/SearchPlateForm.jsx
+++ src/routes/Dashboard/SearchPlateForm.jsx
@@ -1,9 +1,11 @@
 import {useState} from "react";
 import Button from "components/main/Button";
+import ButtonDelete from "components/main/ButtonDelete";
 
-export default function SearchPlateForm() {
+export default function SearchPlateForm({handleSearchPlate}) {
     const [inputValue, setInputValue] = useState('')
     const [isDeleting, setIsDeleting] = useState(false)
+    const [showButtonDelete, setShowButtonDelete] = useState(false)
 
     const handleKeyDelete = (e) => e.key === 'Backspace' && setIsDeleting(true)
 
@@ -22,15 +24,24 @@ export default function SearchPlateForm() {
     }
 
     return (
-        <form className="flex flex-col">
+        <form className="flex flex-col"
+              onSubmit={e=>
+                {
+                    e.preventDefault()
+                    if(inputValue.length===7) {
+                        handleSearchPlate(inputValue)
+                        setShowButtonDelete(true)
+                    }
+                }}>
             <input
-                className="rounded-md text-center text-6xl text-gray-800 font-bold mb-4 placeholder:text-gray-200 border-gray-100"
+                className="relative rounded-md text-center text-6xl text-gray-800 font-bold mb-4 placeholder:text-gray-200 border-gray-100"
                 type="text"
                 placeholder="PLACA"
                 value={inputValue}
                 onChange={handleInputValue}
                 onKeyDown={handleKeyDelete}
             />
+            {showButtonDelete && <ButtonDelete className={"absolute top-[-16px]"}/>}
             <Button
                 fullWidth>
                 Buscar

1466506fadbfe50f7dd94498043e1cad663814ff
jbayron09 <j.bayron.b@gmail.com>
2022-10-14 11:36:57 -0500

    rendering the difference between two dates and times

--- src/routes/Dashboard/SummaryCard/index.jsx
+++ src/routes/Dashboard/SummaryCard/index.jsx
@@ -1,15 +1,24 @@
 import {BiTime, BiDollarCircle} from "react-icons/bi";
 import Button from "components/main/Button";
 import SummaryCardSection from "routes/Dashboard/SummaryCard/components/SummaryCardSection";
+import {DateTime} from "luxon";
 
 export default function SummaryCard() {
+
+    const date1 = DateTime.fromISO("2022-10-15T08:35:05")
+    const date2 = DateTime.fromISO("2022-10-14T07:00:00")
+
+    const diff = date1.diff(date2, ["months", "days", "hours", "minutes","seconds"]).toObject()
+
+
     return (
         <div className="bg-white rounded-lg">
             <div className="p-8">
                 <SummaryCardSection
                     icon={BiTime}
                     className="mb-2">
-                    Aún no está contando el tiempo
+                    {/*Aún no está contando el tiempo*/}
+                    {diff.hours + " hora, " + diff.minutes + " minutos, " + diff.seconds + " segundos"}
                 </SummaryCardSection>
                 <SummaryCardSection icon={BiDollarCircle}>
                     $0

10a9430d1b71fbfd1cca92a0a59d3c1b37a3af56
jbayron09 <j.bayron.b@gmail.com>
2022-10-18 14:50:29 -0500

    the default value of the addcoma is arranged

--- src/routes/Dashboard/SummaryCard/components/CounterSection.jsx
+++ src/routes/Dashboard/SummaryCard/components/CounterSection.jsx
@@ -3,8 +3,9 @@ import PropTypes from "prop-types";
 export default function CounterSection({value, label, addComma}) {
     value = Math.ceil(value)
     if (!value) return null
-    return `${value} ${(value > 1) ? label + 's' : label}${addComma&&', '}`;
+    return `${value} ${(value > 1) ? label + 's' : label}${addComma ? ', ' : ''}`;
 }
+
 CounterSection.propTypes = {
     label: PropTypes.oneOf(['dia', 'hora', 'minuto', 'segundo']),
     value: PropTypes.number,
@@ -12,5 +13,5 @@ CounterSection.propTypes = {
 }
 
 CounterSection.defaultProps = {
-    addComma: ''
+    addComma: false
 }

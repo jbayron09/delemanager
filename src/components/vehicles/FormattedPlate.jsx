f7bec79a7f5aa6e537df099b5d3e84bbc2e8c83a
Cristian Rojas <cristianguillermorojas@gmail.com>
2022-10-27 17:24:51 -0500

    Improved styling to make all them be te same width

--- src/components/vehicles/FormattedPlate.jsx
+++ src/components/vehicles/FormattedPlate.jsx
@@ -1,10 +1,28 @@
 import PropTypes from 'prop-types'
 import { formatPlate } from "utils/vehicles/plates"
+import classNames from "classnames"
 
-export default function FormattedPlate({plate}) {
+export default function FormattedPlate({ plate }) {
+  const formatted = formatPlate(plate)
+
   return (
       <span>
-        {formatPlate(plate)}
+        {
+          Array.from(formatted).map((letter, index) => {
+            const isSeparator = letter === ' '
+
+            return (
+                <span
+                    key={index}
+                    className={classNames([
+                      'inline-block w-3',
+                      isSeparator && 'w-2'
+                    ])}>
+                {letter}
+              </span>
+            )
+          })
+        }
       </span>
   )
 }
